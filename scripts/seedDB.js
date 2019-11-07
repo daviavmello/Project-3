dd86c5ad742838e52a93d3bc9357fb39738907e6
m3llington <mellingtonucf@knights.ucf.edu>
2019-11-06 21:34:31 -0500

    have the seeds working

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,3 +1,15 @@
+const mongoose = require("mongoose");
+const db = require("../models/Budget");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/mern-passport"
+);
+
+
+
 const budgetSeed = [
     {
       item: "Apple",
@@ -10,3 +22,15 @@ const budgetSeed = [
       "10.00"
     }
 ];
+
+
+  db.remove({})
+  .then(() => db.collection.insertMany(budgetSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
