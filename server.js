2cba7b070939fb971034df107c6b58b7ae9969e1
Davi Mello <davimello1994@gmail.com>
2019-11-16 09:42:01 -0500

    Added path to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
